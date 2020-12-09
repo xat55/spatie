d36e26037d478a49748d0331cc494e91aa184b94
xat55 <xat55@mail.ru>
2020-12-09 13:05:08 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    
+    // Role::create(['name' => 'writer']);
+    // Permission::create(['name' => 'edit articles']);
+    // return 'Role created...';
+    
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
