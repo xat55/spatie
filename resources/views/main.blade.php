55fc134aeee03ca92ed514bff2d655a5bf1c5223
xat55 <xat55@mail.ru>
2020-12-10 21:57:34 +0300

    Change

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -21,6 +21,7 @@
         @endforeach  
       </div><!-- /.blog-post -->
       
+      
       <div class="blog-post">
         <h2 class="blog-post-title">Another blog post</h2>
         <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
