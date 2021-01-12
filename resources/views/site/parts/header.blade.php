1e31982322d229491dc5e7e8d57e4718d0b6bfb7
xat55 <xat55@mail.ru>
2021-01-12 21:25:54 +0300

    Added anchor

--- resources/views/site/parts/header.blade.php
+++ resources/views/site/parts/header.blade.php
@@ -1,4 +1,4 @@
-<header class="blog-header py-3">
+<header class="blog-header py-3" name="top">
     <div class="row flex-nowrap justify-content-between align-items-center">                    
         <div class="col-4 pt-1">
             <a class="text-muted" href="{{ route('posts.create') }}">Разместить статью</a>
