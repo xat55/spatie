5bda101ac939f7d7c4f32c8ea2bc14d749ac7c02
xat55 <xat55@mail.ru>
2021-01-13 15:13:03 +0300

    Added 'banned' condition output posts

--- resources/views/site/parts/content.blade.php
+++ resources/views/site/parts/content.blade.php
@@ -1,24 +1,27 @@
 <div class="col-md-8 blog-main">
-
-  <h3 class="pb-4 mb-4 font-italic border-bottom">
-    From the Firehose
-  </h3>  
-  
-  <div class="blog-post">    
-    @foreach($posts as $post)            
-      <a href="{{ route('single_post', $post) }}"><h4 class="blog-post-title">{{ $post->header }}</h4></a>
-      <p class="blog-post-meta">
-        {{ $post->getPostDate() }} by        
-        <a href="{{ route('show_posts_author', $post->user_id) }}">{{ $post->user->name }}, </a>             
-        рубрика: {{ $post->categories->implode('name', ', ') }}
-      </p>          
-      <p>{{ Str::limit($post->text, 80, '... [') }}         
-             <a href="{{ route('single_post', $post) }}"><small>Read more</small></a>    
-          ]
-      </p>  
-    @endforeach
-  </div><!-- /.blog-post -->
-  
-  {{ $posts->links() }}
-  
+    
+    <h3 class="pb-4 mb-4 font-italic border-bottom">
+        From the Firehose
+    </h3>  
+    
+    <div class="blog-post">    
+        @foreach($posts as $post)            
+            
+            @if(!$post->user->hasRole('banned'))
+                <a href="{{ route('single_post', $post) }}"><h4 class="blog-post-title">{{ $post->header }}</h4></a>
+                <p class="blog-post-meta">
+                    {{ $post->getPostDate() }} by        
+                    <a href="{{ route('show_posts_author', $post->user_id) }}">{{ $post->user->name }}, </a>             
+                    рубрика: {{ $post->categories->implode('name', ', ') }}
+                </p>          
+                <p>{{ Str::limit($post->text, 80, '... [') }}         
+                    <a href="{{ route('single_post', $post) }}"><small>Read more</small></a>    
+                    ]
+                </p>  
+            @endif
+        @endforeach
+    </div><!-- /.blog-post -->
+    
+    {{ $posts->links() }}
+    
 </div><!-- /.blog-main -->
